3cab993b54639a68a9bcff7377598dfa82bd25af
Akiva Miura <akiva.miura@gmail.com>
2025-06-08 20:49:17 +0900

    Update install-x64-1.0.0.sh to change AppImage URL to a new download location

--- install-x64-1.0.0.sh
+++ install-x64-1.0.0.sh
@@ -2,7 +2,8 @@
 
 set -eu
 
-APPIMAGE_URL="https://github.com/cursor-desktop/cursor-desktop/releases/download/1.0.0/Cursor-1.0.0-x86_64.AppImage"
+#APPIMAGE_URL="https://github.com/cursor-desktop/cursor-desktop/releases/download/1.0.0/Cursor-1.0.0-x86_64.AppImage"
+APPIMAGE_URL="https://downloads.cursor.com/production/53b99ce608cba35127ae3a050c1738a959750865/linux/x64/Cursor-1.0.0-x86_64.AppImage"
 
 # Get the directory path of this script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
